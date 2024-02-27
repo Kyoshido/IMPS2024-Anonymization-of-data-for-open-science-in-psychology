3a8d562c31109a26b88c5f95b300aae4f7926f40
Jiří Novák <32874621+Kyoshido@users.noreply.github.com>
2024-02-27 06:12:48 +0100

    Update

---
+++ Analysis/script.R
@@ -0,0 +1,56 @@
+###############################################################################
+###############################################################################
+###############################################################################
+
+library(psych)
+library(psychTools)
+
+data(Thurstone)
+dim(Thurstone) # small
+
+data(bfi)
+dim(bfi) 
+# 25 personality self report items taken from the International Personality Item 
+# Pool (ipip.ori.org) were included as part of the Synthetic Aperture Personality 
+# Assessment (SAPA) web based personality assessment project. The data from 2800
+# subjects are included here as a demonstration set for scale construction, 
+# factor analysis and Item Response Theory analyses.
+
+data(spi)
+dim(spi) # ???
+
+data(sat.act)
+dim(sat.act) # small
+
+data(epi.bfi)
+dim(epi.bfi) # ???
+
+data(epiR)
+dim(epiR) # ???
+
+
+
+data(iqitems) # ???
+dim(iqitems) # ??? 
+
+data(ability) # ??? 
+dim(ability) # ??? 
+
+data(galton)
+dim(galton) # ??? 
+
+data(Dwyer)
+dim(Dwyer) # small
+
+data(miscellaneous)
+dim(miscellaneous) # ??? 
+
+
+
+
+
+
+
+###############################################################################
+###############################################################################
+###############################################################################
